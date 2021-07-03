615c5e8f1e65c83b24f54b801d110ce60f048f8f
barrystyle <barrystyle@westnet.com.au>
2021-07-03 14:37:43 +0800

    prevent assert in wallet code for coinstakes

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -5511,7 +5511,8 @@ int CMerkleTx::GetBlocksToMaturity() const
     if (!IsCoinBase() && !IsCoinStake())
         return 0;
     int chain_depth = GetDepthInMainChain();
-    assert(chain_depth >= 0); // coinbase tx should not be conflicted
+    if (!IsCoinStake())
+        assert(chain_depth >= 0); // coinbase tx should not be conflicted
     return std::max(0, (COINBASE_MATURITY+1) - chain_depth);
 }
 
