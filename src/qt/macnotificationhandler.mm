a2e286392100f398fe43c71a6d94ecd453fb416a
barrystyle <barrystyle@westnet.com.au>
2022-06-13 20:58:09 +0800

    update .gitignore and text references

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -13,7 +13,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"org.dash.Dash-Qt";
+        return @"org.dash.Pacprotocol-Qt";
     } else {
         return [self __bundleIdentifier];
     }
